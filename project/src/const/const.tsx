23ab9e6c18a30262fa361c8cc25bceb37ee41ed7
Николай <rukin.nik.1996@mail.ru>
2022-11-13 18:19:03 +0700

    заменил иконки

--- project/src/const/const.tsx
+++ project/src/const/const.tsx
@@ -39,7 +39,7 @@ export const OfferOnMain: offerCardConfig = {
 };
 
 export const URL_MARKER_DEFAULT =
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/pin.svg';
+  '../../../img/pin.svg';
 
 export const URL_MARKER_CURRENT =
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/main-pin.svg';
+  '../../../img/pin-active.svg';
